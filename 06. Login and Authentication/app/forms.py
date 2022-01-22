d9771cc5efd2ee8d85296ebfcd52df80bbc65d93
zubairwazir <zubairwazir777@gamil.com>
2022-01-22 16:12:28 +0500

    updating files

--- 06. Login and Authentication/app/forms.py
+++ 06. Login and Authentication/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+    # Custom validation for username
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username already exits!')
+
+    # Custom validation for email
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email already exits!')
 
 
 class LoginForm(FlaskForm):
