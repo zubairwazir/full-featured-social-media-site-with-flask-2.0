13511af8f2326fe2b40513c74cb77cbd9ee4f4f5
Zubair Ahmad <zubairwazir777@gmail.com>
2022-01-22 11:54:50 +0500

    Revert "adding files"

--- 06. Login and Authentication/app/models.py
+++ 06. Login and Authentication/app/models.py
@@ -1,13 +1,8 @@
+from app import db
 from datetime import datetime
-from app import db, login_manager
-from flask_login import UserMixin
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
--- 06. Login and Authentication/app/routes.py
+++ 06. Login and Authentication/app/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, url_for, redirect, flash
-from app import app, db, bcrypt
+from app import app
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
-from flask_login import  login_user, logout_user, current_user
 
 
 posts = [
@@ -33,35 +32,20 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registered Successfully! Please Login Now.', 'success')
-        return redirect(url_for('login'))
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'admin':
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful! Please check Email and Password.', 'danger')
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
